5967f907f49bd60fb0c9d088a5f5ebe5a5f68812
jaikrishann <jaikrishan2001@gmail.com>
2025-01-24 12:11:44 +0530

    logging and execption setup done

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from source_code.logger import logging
-logging.info("hello world")
-logging.debug("hello world")
-logging.warning("hello world")
-logging.critical("hello world")
-logging.error("hello world")
+from source_code.execption import InsuranceException
+import sys 
+
+# logging.info("hello world")
+# logging.debug("hello world")
+# logging.warning("hello world")
+# logging.critical("hello world")
+# logging.error("hello world")
+
+
+try:
+    10/0
+except Exception as e:
+
+    obj =  InsuranceException(error_message=e,error_details= sys )
+    logging.warning(obj.error_message)
+    print(obj.error_message)
+
